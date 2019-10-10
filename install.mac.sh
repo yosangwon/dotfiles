53251f644ce10102f43d582062c909ae56283764
YI Sangwon <yi@sangwon.dev>
2019-10-10 09:22:37 +0900

    Conditional stuff not to install brew over and over again

--- install.mac.sh
+++
@@ -1,9 +0,0 @@
-# install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install Vundlevim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# install other usual stuff
-brew install nvim tmux zsh ffmpeg antigen chruby ruby-install ag
-brew cask install google-cloud-sdk
