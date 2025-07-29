59215c374885ab3ac691fe2f2a8b45fb51a46741
Sangwon Yi <hello@sangwon.dev>
2025-07-29 11:33:43 +0900

    tired of lots of nvim plugin updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -31,4 +31,5 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  git = { cooldown = 60 * 60 * 24 * 7 },
 })
